a00af06a592839df563ac02e5106c952ba7a4888
ThatsNotMe <mineflax@gmail.com>
2024-09-02 15:41:06 +0200

    Added Basic Controls

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour {
 
-    public Board board {  get; private set; }
-    public Vector3Int position {  get; private set; }
+    public Board board { get; private set; }
+    public Vector3Int position { get; private set; }
     public TetrominoData data { get; private set; }
 
     public Vector3Int[] cells { get; private set; }
@@ -19,7 +19,39 @@ public class Piece : MonoBehaviour {
         }
 
         for (int i = 0; i < data.cells.Length; i++) {
-            this.cells[i] = (Vector3Int) data.cells[i];
+            this.cells[i] = (Vector3Int)data.cells[i];
         }
+    }
+
+    private void Update() {
+
+        this.board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Move(Vector2Int.left);
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            Move(Vector2Int.down);
+        }
+
+        this.board.Set(this);
+
+    }
+
+    private bool Move(Vector2Int translation) {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this, newPosition);
+
+        if (valid) {
+            this.position = newPosition;
+        }
+
+        return valid;
     }
 }
